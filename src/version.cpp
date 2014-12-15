0e0bb35ab2fd33884ec2104a81e59a90cba454d6
Crypto Jet <cryptojet@yahoo.com>
2014-12-14 19:41:01 -0600

    Dotcoin

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both kimdotcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Dotcoin DOT - The official coin of Cryptopia");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
