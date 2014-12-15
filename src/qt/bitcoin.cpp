8df2be934ee220c2e46f1e12d8dc68d6b6a8a52a
Crypto Jet <cryptojet@yahoo.com>
2014-12-14 19:36:17 -0600

    Dotcoin

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -150,9 +150,9 @@ int main(int argc, char *argv[])
 
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
-    QApplication::setOrganizationName("Kimdotcoin");
-    QApplication::setOrganizationDomain("coingen.io");
-    QApplication::setApplicationName("Kimdotcoin-Qt");
+    QApplication::setOrganizationName("Dotcoin");
+    QApplication::setOrganizationDomain("Cryptopia.co.nz");
+    QApplication::setApplicationName("Dotcoin-Qt");
 
     // ... then GUI settings:
     OptionsModel optionsModel;
